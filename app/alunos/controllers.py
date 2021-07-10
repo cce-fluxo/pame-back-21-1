3dadbf5a44a7af02de844f4904bad1e06b8ccb2a
Camila Maia <camilamaia@poli.ufrj.br>
2021-07-10 11:58:06 -0300

    Adiconando comentários

--- app/alunos/controllers.py
+++ app/alunos/controllers.py
@@ -1,10 +1,14 @@
 from flask import  request, jsonify
-from flask.views import MethodView
+from flask.views import MethodView #Faz com que a classe seja vista como um método para requisição HTTP
 from app.alunos.model import Aluno
 from app.extensions import db
 from flask_jwt_extended import create_access_token
 
-class AlunosCreate(MethodView):      # /aluno/create
+#As rotas vão ser acessadas quando o front fizer uma requisição para o backend
+#No python é mais interessante trabalhar com classes do que com funções, até por causa do quesito segurança
+#e também porque ajudam a simplificar a modelagem dos dados e as ações em programas
+
+class AlunosCreate(MethodView):      #/aluno/create
     def get(self):
         aluno = Aluno.query.all()
         return jsonify([aluno.json() for aluno in aluno]),200
@@ -25,7 +29,7 @@ class AlunosCreate(MethodView):      # /aluno/create
 
         return aluno.json() , 200
 
-class AlunosDetails(MethodView):      # /aluno/details/<int:id>
+class AlunosDetails(MethodView):      #/aluno/details/<int:id>
     def get(self,id):
         aluno = Aluno.query.get_or_404(id)
         return aluno.json(),200
--- app/alunos/routes.py
+++ app/alunos/routes.py
@@ -1,12 +1,16 @@
 from flask import Blueprint
 
 from app.alunos.controllers import AlunosCreate, AlunosDetails, AlunoLogin
+# 1 ponto- arquivos de dentro dessa pasta (.controllers)
+# 2 pontos- fora da pasta 
 
 aluno_api = Blueprint('aluno_api', __name__)
 
 aluno_api.add_url_rule(
     '/aluno/create', view_func= AlunosCreate.as_view('aluno_create'), methods=['GET','POST']
 )
+
+#view_func vem do MethidView 
 
 aluno_api.add_url_rule(
     '/aluno/details/<int:id>', view_func= AlunosDetails.as_view('aluno_details'), methods=['GET','PUT','PATCH','DELETE']
